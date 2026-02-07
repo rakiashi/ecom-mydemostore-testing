58f216c2dd93189d1b884e82684f1f3fbdb5dd16
rakesh <rakesh.m3106@gmail.com>
2026-02-07 16:37:22 +0100

    slack-notify-CI

--- ui-tests/tests/product-details/pdp.test.ts
+++ ui-tests/tests/product-details/pdp.test.ts
@@ -1,6 +1,12 @@
 import { test } from "../../fixtures/app-page";
 
-test("User sees all PDP elements", async ({ plpPage, pdpPage }) => {
-  await plpPage.openProductByIndex(1);
-  await pdpPage.expectLoaded();
-});
+test.describe(
+  "Plp Functional Test",
+  { tag: ["@Plp", "@FullRegression"] },
+  () => {
+    test("User sees all PDP elements", async ({ plpPage, pdpPage }) => {
+      await plpPage.openProductByIndex(1);
+      await pdpPage.expectLoaded();
+    });
+  },
+);
--- ui-tests/tests/snapshots/visual.test.ts
+++ ui-tests/tests/snapshots/visual.test.ts
@@ -1,28 +1,23 @@
 import { test, expect } from "../../fixtures/app-page";
 
-test.describe(
-  "visual Testing",
-  { tag: ["@Snapshot"] },
-  () => {
+test.describe("visual Testing", { tag: ["@Snapshot"] }, () => {
   test("Verify UI Design and CSS Consistency with Snapshot Testing on Plp Page", async ({
     page,
   }) => {
-   const plpSection = page.locator("section");
-   await expect(plpSection).toHaveScreenshot(
-    "plp-ui-snapshot-desktop-chromium.png"
-  );
+    const plpSection = page.locator("section");
+    await expect(plpSection).toHaveScreenshot(
+      "plp-ui-snapshot-desktop-chromium.png",
+    );
   });
-   
-   test("Verify UI Design and CSS Consistency with Snapshot Testing on Pdp Page", async ({
-    page,plpPage
+
+  test("Verify UI Design and CSS Consistency with Snapshot Testing on Pdp Page", async ({
+    page,
+    plpPage,
   }) => {
     await plpPage.openProductByIndex(1);
     await page.waitForLoadState("domcontentloaded");
     const component = page.locator('[class="product-detail"]');
     const pdpSection = await component.screenshot();
-    expect(pdpSection).toMatchSnapshot(
-       "pdp-ui-snapshot-desktop-chromium.png"
-     );
+    expect(pdpSection).toMatchSnapshot("pdp-ui-snapshot-desktop-chromium.png");
   });
-
-  });
+});
